c15f14806b1b7554a55dcc9e72a554179d436fde
asadmmn <asadm8975@gmail.com>
2025-08-18 13:25:58 +0500

    new host updated

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // local development
-export const HOST_API ="http://127.0.0.1:8000/api/";
+// export const HOST_API ="http://127.0.0.1:8000/api/";
 
 export const PUSHER_HOST ="127.0.0.1";
 
@@ -8,4 +8,4 @@ export const PUSHER_HOST ="127.0.0.1";
 
 
 // new server
-//  export const HOST_API="https://portal.grapetask.co/api/";
+ export const HOST_API="https://portal.grapetask.co/api/";
